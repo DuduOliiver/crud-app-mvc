7bcef6f3c2ee563204bff7eaec6e462528ec2519
DuduOliiver <eduardoliveira.11@hotmail.com>
2021-04-25 01:25:40 -0300

    commit

---
+++ Models/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public class Produto : Entity
+    {
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "Campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        [StringLength(1000, ErrorMessage = "Campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
+
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "Campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
+
+        public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        public decimal Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
